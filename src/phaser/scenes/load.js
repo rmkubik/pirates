572f62b82f55117e6781eca8366a22babf673fe5
Ryan Kubik <rmkubik@me.com>
2018-12-22 16:04:51 -0800

    Import kenney pirate assets

--- src/phaser/scenes/load.js
+++ src/phaser/scenes/load.js
@@ -1,12 +1,22 @@
 import Phaser from 'phaser';
 
+import shipsSheet from '../../../assets/spritesheets/shipsMiscellaneous_sheet@2.png';
+import shipsXml from '../../../assets/spritesheets/shipsMiscellaneous_sheet@2.xml';
+import tilesSheet from '../../../assets/spritesheets/tiles_sheet@2.png';
 class LoadScene extends Phaser.Scene {
   constructor() {
     super({ key: 'load' });
   }
 
   preload() {
+    this.load.spritesheet('tiles', tilesSheet, {
+      frameWidth: 16,
+      frameHeight: 16,
+      spacing: 1,
+      margin: 1,
+    });
 
+    this.load.atlasXML('ships', shipsSheet, shipsXml);
   }
 
   create() {
