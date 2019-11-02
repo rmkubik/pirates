098d52bc205d25c5f4e9216186e4d280246e1f21
Ryan Kubik <rmkubik@me.com>
2019-11-01 20:36:20 -0700

    Create rudder

--- src/phaser/prefabs/ship.js
+++ src/phaser/prefabs/ship.js
@@ -1,8 +1,16 @@
-import Phaser from 'phaser';
+import Phaser from "phaser";
 
 class Ship {
   constructor({
-    scene, x, y, key, frame, sail
+    scene,
+    x,
+    y,
+    key,
+    frame,
+    sail,
+    hullFrame,
+    sailFrame,
+    rudderFrame
   }) {
     const { Vector } = Phaser.Physics.Matter.Matter;
     // this.bodyShape = {
@@ -14,8 +22,36 @@ class Ship {
     //     y: 4,
     //   }
     // };
-    this.sprite = scene.matter.add.sprite(x, y, key, frame); //, this.bodyShape);
+    this.shipParts = [];
+    // this.sprite = scene.matter.add.sprite(x, y, key, frame); //, this.bodyShape);
+    this.hullSprite = scene.matter.add.sprite(x, y, key, hullFrame);
+    this.sailSprite = scene.add.sprite(x, y, key, sailFrame);
+    this.rudderSprite = scene.add.sprite(x, y, key, rudderFrame);
+    this.rudderSprite.setOrigin(0.95, 0.5);
+    this.shipParts.push(this.hullSprite);
+    this.shipParts.push(this.sailSprite);
     this.sail = Vector.create(sail.x, sail.y);
+
+    this.rudderDirection; // controls rotation
+    this.sailDirection; // controls speed
+    this.shipDirection; // controls direction
+  }
+
+  update() {
+    this.sailSprite.x = this.hullSprite.x;
+    this.sailSprite.y = this.hullSprite.y;
+
+    // this.sailSprite.angle =
+    //   Math.atan2(this.sail.y, this.sail.x) * (180 / Math.PI);
+
+    this.rudderSprite.x = this.hullSprite.x;
+    this.rudderSprite.y = this.hullSprite.y - this.hullSprite.height / 2;
+
+    const { Vector } = Phaser.Physics.Matter.Matter;
+
+    const rudderDirection = Vector.perp(this.sail, true);
+    this.rudderSprite.angle =
+      Math.atan2(rudderDirection.y, rudderDirection.x) * (180 / Math.PI);
   }
 }
 
--- src/phaser/scenes/main.js
+++ src/phaser/scenes/main.js
@@ -1,16 +1,14 @@
-import Phaser from 'phaser';
+import Phaser from "phaser";
 
-import Map from '../map/map';
-import Ship from '../prefabs/ship';
+import Map from "../map/map";
+import Ship from "../prefabs/ship";
 
 class MainScene extends Phaser.Scene {
   constructor() {
-    super({ key: 'main' });
+    super({ key: "main" });
   }
 
-  preload() {
-
-  }
+  preload() {}
 
   create() {
     const map = new Map({
@@ -18,26 +16,36 @@ class MainScene extends Phaser.Scene {
       height: 10,
       width: 10,
       tileHeight: 128,
-      tileWidth: 128,
+      tileWidth: 128
     });
 
     this.objects = [];
-    this.objects.push(new Ship({
-      scene: this,
-      x: 100,
-      y: 220,
-      key: 'ships',
-      frame: 'ship (4).png',
-      sail: { x: -1, y: 1 },
-    }));
-    this.objects.push(new Ship({
-      scene: this,
-      x: 560,
-      y: 220,
-      key: 'ships',
-      frame: 'ship (5).png',
-      sail: { x: -1, y: 0 },
-    }));
+    this.objects.push(
+      new Ship({
+        scene: this,
+        x: 100,
+        y: 220,
+        key: "ships",
+        frame: "ship (4).png",
+        hullFrame: "hullLarge (1).png",
+        sailFrame: "sailLarge (10).png",
+        rudderFrame: "wood (4).png",
+        sail: { x: -1, y: 1 }
+      })
+    );
+    this.objects.push(
+      new Ship({
+        scene: this,
+        x: 560,
+        y: 220,
+        key: "ships",
+        frame: "ship (5).png",
+        hullFrame: "hullLarge (1).png",
+        sailFrame: "sailLarge (11).png",
+        rudderFrame: "wood (4).png",
+        sail: { x: -1, y: 0 }
+      })
+    );
 
     const { Vector } = Phaser.Physics.Matter.Matter;
     const direction = Vector.create(0, 1);
@@ -45,17 +53,22 @@ class MainScene extends Phaser.Scene {
     const forceMagnitude = 0.0001;
     const force = Vector.mult(normal, forceMagnitude);
 
-    this.matter.world.on('beforeupdate', () => {
-      this.objects.forEach((object) => {
+    this.matter.world.on("beforeupdate", () => {
+      this.objects.forEach(object => {
         const sailNormal = Vector.normalise(Vector.perp(object.sail, true));
         const sailForce = Vector.mult(sailNormal, forceMagnitude);
-        object.sprite.applyForce(sailForce);
+        // object.shipParts.forEach(sprite => sprite.applyForce(sailForce));
+        object.hullSprite.applyForce(sailForce);
         // TODO: Apply a maximum velocity somehow
         // TODO: Apply some friction from the water to slow down boat if sails put up
         // TODO: Apply a fraction of the force depending on the angle of the sails
         // TODO: Apply the force fraction in the direction of the sails
       });
     });
+  }
+
+  update() {
+    this.objects.forEach(object => object.update());
   }
 }
 
